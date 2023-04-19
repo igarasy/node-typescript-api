a590a9449634ff15f325c2f9a8ddcaa814ac0705
igarasy <lucas.igarashi@caju.com.br>
2023-04-19 17:47:45 -0300

    chore: initiate project

---
+++ src/util/module-alias.ts
@@ -0,0 +1,11 @@
+import * as path from 'path';
+import moduleAlias from 'module-alias';
+
+//this will catch the actual directory, go back 2 folders and import all files
+const files = path.resolve(__dirname, '../..');
+
+
+moduleAlias.addAliases({
+  '@src': path.join(files, 'src'),
+  '@test': path.join(files, 'test'),
+});
