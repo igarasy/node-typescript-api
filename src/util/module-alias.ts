1f50a75cadc4775128ccee6c24a6fb9648e82692
igarasy <lucas.igarashi@caju.com.br>
2023-04-20 14:20:29 -0300

    chore: add prettier

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -4,7 +4,6 @@ import moduleAlias from 'module-alias';
 //this will catch the actual directory, go back 2 folders and import all files
 const files = path.resolve(__dirname, '../..');
 
-
 moduleAlias.addAliases({
   '@src': path.join(files, 'src'),
   '@test': path.join(files, 'test'),
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,11 +1,11 @@
 //this file will initialize the server for all functional tests
-import { SetupServer } from '@src/server'
-import supertest from 'supertest'
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
 
 beforeAll(() => {
-  const server = new SetupServer()
-  server.init()
+  const server = new SetupServer();
+  server.init();
 
   //this will set globally this server in jest
-  global.testRequest = supertest(server.getApp())
-})
+  global.testRequest = supertest(server.getApp());
+});
